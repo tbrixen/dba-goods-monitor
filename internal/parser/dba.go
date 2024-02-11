c6f7f6fe08e8a07e24324c43bdb2c52d28ce2d5c
Tobias Brixen <tbrixen@gmail.com>
2024-02-11 21:55:14 +0100

    Add parser to test if listing is still active

--- internal/parser/dba.go
+++ internal/parser/dba.go
@@ -1,6 +1,11 @@
 package parser
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+
+	"github.com/gocolly/colly"
+)
 
 type Dba struct {
 }
@@ -9,6 +14,31 @@ func NewDba() Dba {
 	return Dba{}
 }
 
-func (d *Dba) IsActive(url string) {
+func (d *Dba) IsActive(url string) bool {
 	fmt.Println("isActive called from parser.dba")
+	var isActive bool
+
+	c := colly.NewCollector()
+
+	c.OnHTML("body", func(e *colly.HTMLElement) {
+		e.ForEach("span", func(_ int, el *colly.HTMLElement) {
+			if strings.Contains(el.Text, "Skriv til sælger") {
+				isActive = true
+			}
+		})
+		e.ForEach("div", func(_ int, el *colly.HTMLElement) {
+			if strings.Contains(el.Text, "Denne annonce er inaktiv") {
+				isActive = false
+			}
+		})
+	})
+
+	// Before making a request print "Visiting ..."
+	c.OnRequest(func(r *colly.Request) {
+		fmt.Println("Visiting", r.URL.String())
+	})
+
+	c.Visit(url)
+
+	return isActive
 }
--- internal/parser/dba_test.go
+++ internal/parser/dba_test.go
@@ -1,8 +1,31 @@
 package parser
 
-import "testing"
+import (
+	"testing"
 
-func TestIsAct(t *testing.T) {
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSoldItemsThatRedirects(t *testing.T) {
 	dba := NewDba()
-	dba.IsActive("foo")
+
+	result := dba.IsActive("https://www.dba.dk/annonce/id-1110121538")
+
+	assert.False(t, result)
+}
+
+func TestInactiveListingPage(t *testing.T) {
+	dba := NewDba()
+
+	result := dba.IsActive("https://www.dba.dk/prime-fuji-xf-56mm-f12/id-1110117857/")
+
+	assert.False(t, result)
+}
+
+func TestStillActive(t *testing.T) {
+	dba := NewDba()
+
+	result := dba.IsActive("https://www.dba.dk/airpods-max-med-smart-case/id-1000455752/")
+
+	assert.True(t, result)
 }
